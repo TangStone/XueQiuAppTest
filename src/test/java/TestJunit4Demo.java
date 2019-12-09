086f1ca01058ae06f94214a0f0467f54af1f911e
TangStone <18845599643@163.com>
2019-12-09 15:45:32 +0800

    fist testcase

--- src/test/java/TestJunit4Demo.java
+++ src/test/java/TestJunit4Demo.java
@@ -1,6 +1,15 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestJunit4Demo {
     @Test
+    public void testDemo(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testDemo1(){
+        Assert.assertTrue(false);
+    }
 
 }
